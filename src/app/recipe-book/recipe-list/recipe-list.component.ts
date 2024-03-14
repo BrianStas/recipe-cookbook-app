ac064b1f4ef94ed05e0827f3ff5516db4e88bb2a
Brian Stasiukaitis <brianstas@gmail.com>
2024-03-14 17:47:34 -0400

    Added initial layout
    Added in initial blocking to make the site have all features represented though not yet working as expected.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'Pizza is a popular Italian dish', 'https://img.cdn4dd.com/cdn-cgi/image/fit=cover,width=600,height=400,format=auto,quality=80/https://doordash-static.s3.amazonaws.com/media/store/header/e4e3949f-384f-42ea-b138-44d94c82cffe.jpeg')
+  ];
 
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+  ingredients: Ingredient[] = [
+    new Ingredient('Eggs', 2),
+    new Ingredient('Milk', 1),
+    new Ingredient('Bread', 6),
+    new Ingredient('Butter', 1),
+    new Ingredient('Sugar', 1),
+    new Ingredient('Coffee', 5)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 
 }
