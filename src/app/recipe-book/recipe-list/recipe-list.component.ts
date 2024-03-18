fba29ae4bf35ec17954abdb6da28f7ef06ce73ea
Brian Stasiukaitis <brianstas@gmail.com>
2024-03-18 16:23:50 -0400

    More work on preliminary shopping
    Shopping list technically works, though not through using a form. Last commit for now, will come back after learning about Directives

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
     new Recipe('Pizza', 'Pizza is a popular Italian dish', 'https://img.cdn4dd.com/cdn-cgi/image/fit=cover,width=600,height=400,format=auto,quality=80/https://doordash-static.s3.amazonaws.com/media/store/header/e4e3949f-384f-42ea-b138-44d94c82cffe.jpeg'),
     new Recipe('Pasta', 'Pasta is a popular Italian dish', 'https://www.allrecipes.com/thmb/gTibTRJ8MW87L0jMhAvXPjIDD94=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/19402-quick-and-easy-alfredo-sauce-DDMFS-4x3-17abb2055c714807944172db9172b045.jpg')
@@ -15,5 +17,9 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
   
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+  this.recipeWasSelected.emit(recipe);
   }
 }
